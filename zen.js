224e28bb87b1dac370894eec20ebe16868f7a584
Seva Rybakov <coding.fu@gmail.com>
2013-04-05 00:23:33 +0300

    Added 403 timeout and notification. Interval is now 1 second

--- zen.js
+++ zen.js
@@ -2,16 +2,24 @@ var request = require('request')
   , _ = require('underscore')
   , messages = []
   , limit = 10000
-  , i = 0;
+  , requests = 0;
 
 function getZen() {
   request('https://api.github.com/zen', function(err, res, body) {
-    if (err || res.statusCode === 403 || i === limit) { return; }
+    ++requests;
+    if (err || requests === limit) { return; }
+
+    if (res.statusCode === 403) {
+      console.log("403. wait.. think.. breathe...");
+      setTimeout(getZen, 5 * 60 * 1000);
+      return;
+    }
+
     if (!_.include(messages, body)) {
       messages.push(body);
       console.log(body);
     }
-    setTimeout(getZen, 300);
+    setTimeout(getZen, 1000);
   });
 }
 
